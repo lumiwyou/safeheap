1f99c547a9f810a7b61db99781aa365b1f29b537
Lumi Hyväri <lumi.hyvari@gmail.com>
2026-04-30 15:23:21 +0000

    New module `secure`: sensitivity classes, secure object protection schemes

---
+++ core/secure/secure.c
@@ -0,0 +1,36 @@
+#include "secure.h"
+
+SecureObjectScheme available_schemes[] = {
+    {
+        TEST,
+        CRITICAL,
+        {
+            // Encryption
+            true
+        },
+        {
+            // Fragmentation
+            true
+        },
+        {
+            // Noising
+            true
+        }
+    },
+};
+
+SecureObjectScheme * get_scheme_by_identifier(SchemeIdentifier id) {
+    for(int s = 0; s < (sizeof(available_schemes) / sizeof(SecureObjectScheme)); s++) {
+        if(id == available_schemes[s].id) {
+            return &available_schemes[s];
+        }
+    }
+}
+
+SchemeIdentifier get_scheme_by_grade(grade, size) {
+    for(int s = 0; s < (sizeof(available_schemes) / sizeof(SecureObjectScheme)); s++) {
+        if(grade == available_schemes[s].sensitivity) {
+            return &available_schemes[s];
+        }
+    }
+}
---
+++ core/secure/secure.h
@@ -0,0 +1,63 @@
+/**
+ * @file secure.h
+ * @brief Internal security scheme and sensitivity management
+ *
+ * Provides functions and definitions for secure schemes and sensitivity classes.
+ *
+ * @author
+ * Lumi Hyväri <lumi.hyvari@gmail.com>
+ *
+ * @copyright
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+#pragma once
+#include <stdbool.h>
+
+typedef void * SecureHandle;
+
+typedef enum {
+    CRITICAL,
+    INTERNAL,
+    PUBLIC
+} Sensitivity;
+
+typedef enum {
+    TEST
+} SchemeIdentifier;
+
+typedef struct {
+    bool enabled;
+    int size_incremental_difference;
+} EncryptionScheme;
+
+typedef struct {
+    bool enabled;
+    int amount;
+} FragmentationScheme;
+
+typedef struct {
+    bool enabled;
+} NoisingScheme;
+
+typedef struct {
+    bool enabled;
+    int rounds;
+    char * pattern;
+    int length;
+} DataWipingScheme;
+
+typedef struct
+{
+    SchemeIdentifier id;
+    Sensitivity sensitivity;
+    EncryptionScheme encryption;
+    FragmentationScheme fragmentation;
+    NoisingScheme noising;
+    DataWipingScheme data_wiping;
+} SecureObjectScheme;
+
+extern SecureObjectScheme available_schemes;
+
+SecureObjectScheme * get_scheme_by_identifier(SchemeIdentifier identifier);
+SchemeIdentifier get_scheme_by_grade(grade, size);
