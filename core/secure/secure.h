84e4801cdc3f136e2415a225dc6ab95e390eadcf
Lumi Hyväri <lumi.hyvari@gmail.com>
2026-05-02 16:14:31 +0000

    Moved individual security mechanism schemes into modules

--- core/secure/secure.h
+++ core/secure/secure.h
@@ -14,6 +14,10 @@
 #pragma once
 #include <stdbool.h>
 #include <stdlib.h>
+#include "encryption/encryption.h"
+#include "fragmentation/fragmentation.h"
+#include "noising/noising.h"
+#include "wiping/wiping.h"
 
 /// @brief The bridging handle between the user-space and the library internals.
 typedef void * SecureHandle;
@@ -25,32 +29,6 @@ typedef enum {
     PUBLIC,
     _DEFAULT
 } Sensitivity;
-
-/// @brief Security sub-scheme for encrypting memory objects.
-typedef struct {
-    bool enabled;
-    int size_incremental_difference;
-} EncryptionScheme;
-
-/// @brief Security sub-scheme for fragmenting memory objects.
-typedef struct {
-    bool enabled;
-    int amount;
-} FragmentationScheme;
-
-/// @brief Security sub-scheme for generating "noise" during input/output operations.
-/// Mitigation against side-channel attacks.
-typedef struct {
-    bool enabled;
-} NoisingScheme;
-
-/// @brief Security sub-scheme for wiping data.
-typedef struct {
-    bool enabled;
-    int rounds;
-    char * pattern;
-    int length;
-} DataWipingScheme;
 
 /// @brief The security scheme for which to use for securing memory objects.
 /// Contains sub-schemes such as encryption, fragmentation, noising, data wiping, etc.
@@ -64,7 +42,7 @@ typedef struct
 } SecureObjectScheme;
 
 /// @brief A list of the available schemes to use
-extern SecureObjectScheme available_schemes[];
+extern SecureObjectScheme security_schemes[];
 
 /// @brief Retrieves security scheme by grade attribute
 /// @param grade Grade to match against
