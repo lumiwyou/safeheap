7f539aa6a860c301e4de42f74f827e57cb3208be
Lumi Hyväri <lumi.hyvari@gmail.com>
2026-05-02 16:15:06 +0000

    Moved scheme into `fragmentation`

--- core/secure/fragmentation/fragmentation.h
+++ core/secure/fragmentation/fragmentation.h
@@ -21,9 +21,15 @@ typedef struct {
     int remainder_size;
 } calculate_fragmentation_result;
 
-/// @brief Utility used for calculating the size of each fragment and the remainder
-/// @param size [in] The size of the memory object
+/// @brief Security sub-scheme for fragmenting memory objects.
+typedef struct {
+    bool enabled;
+    int amount;
+} FragmentationScheme;
+
+/// @brief Utility used for calculating the total_size of each fragment and the remainder
+/// @param total_size [in] The size of the memory object
 /// @param amount [in] The amount of fragments (factor)
 /// @param size_incremental_difference [in] Cryptographical padding to the output
 /// @param result [out] The structure storing the result information
-void calculate_fragmentation(size_t size, int amount, int size_incremental_difference, calculate_fragmentation_result * result);
+void calculate_fragmentation(size_t total_size, int amount, int size_incremental_difference, calculate_fragmentation_result * result);
