95226137f43140ff7f35cf53c8cbdf942904bfff
Lumi Hyväri <lumi.hyvari@gmail.com>
2026-05-02 01:09:51 +0000

    Added doxygen commenting

--- interface/allocate/allocate.h
+++ interface/allocate/allocate.h
@@ -17,7 +17,29 @@
 #include "../../core/secure/wiping/wiping.h"
 #include "../../core/secure/fragmentation/fragmentation.h"
 
+/// @brief Interface function: Allocates a memory object and creates a corresponding `SecureObjectContext`.
+/// Equivalent to stdlib `malloc`
+/// @param size The size of the memory object
+/// @param grade The sensitivity of the memory object
+/// @return A handle to the new context for the object
 SecureHandle sfp_malloc(size_t size, Sensitivity grade);
+
+/// @brief Interface function: Contiguously allocates a new memory object and creates a corresponding `SecureObjectContext`
+/// Equivalent to stdlib `calloc`
+/// @param num Number of items in memory object (such as ints or chars)
+/// @param size The size of the memory object
+/// @param grade The sensitivity of the memory object
+/// @return A handle to the new context for the object
 SecureHandle sfp_calloc(int num, size_t size, Sensitivity grade);
+
+/// @brief Interface function: Resizes the memory object and updates its `SecureObjectContext`
+/// Equivalent to stdlib `realloc`
+/// @param secure_handle The handle to the context
+/// @param size The new size of the memory object
+/// @return New secure handle (resized User Data Buffer)
 SecureHandle sfp_realloc(SecureHandle secure_handle, size_t size);
+
+/// @brief Interface function: Frees the memory object and removes its `SecureObjectContext`
+/// Equivalent to stdlib `free`
+/// @param secure_handle The handle to the context / user data buffer
 void sfp_free(SecureHandle secure_handle);
