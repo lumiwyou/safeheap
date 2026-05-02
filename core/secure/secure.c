22d27c3bfdb8e80d52a5307da8e0dea717919ae7
Lumi Hyväri <lumi.hyvari@gmail.com>
2026-05-02 13:08:43 +0000

    Removed `get_scheme_by_identifier`, `SchemeIdentifier`, and `SecureObjectScheme.id`

--- core/secure/secure.c
+++ core/secure/secure.c
@@ -1,31 +1,6 @@
 #include "secure.h"
 
-SecureObjectScheme available_schemes[] = {
-    {
-        TEST,
-        CRITICAL,
-        {
-            // Encryption
-            true
-        },
-        {
-            // Fragmentation
-            true
-        },
-        {
-            // Noising
-            true
-        }
-    },
-};
-
-SecureObjectScheme * get_scheme_by_identifier(SchemeIdentifier id) {
-    for(int s = 0; s < (sizeof(available_schemes) / sizeof(SecureObjectScheme)); s++) {
-        if(id == available_schemes[s].id) {
-            return &available_schemes[s];
-        }
-    }
-}
+SecureObjectScheme available_schemes[] = {};
 
 SecureObjectScheme * get_scheme_by_grade(Sensitivity grade) {
     for(int s = 0; s < (sizeof(available_schemes) / sizeof(SecureObjectScheme)); s++) {
