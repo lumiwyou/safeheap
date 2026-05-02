f1d6577be52d9fa2c2735319ad4fdd59b2f6acd4
Lumi Hyväri <lumi.hyvari@gmail.com>
2026-05-02 01:09:30 +0000

    Fixed parameter type

--- core/secure/wiping/wiping.c
+++ core/secure/wiping/wiping.c
@@ -1,11 +1,11 @@
 #include "wiping.h"
 
-void overwrite_data(unsigned char * address, size_t length, Sensitivity grade) {
+void overwrite_data(void * address, size_t length, Sensitivity grade) {
     SecureObjectScheme * scheme = get_scheme_by_grade(grade);
     for(int r = 0; r < scheme->data_wiping.rounds; r++) {
         for(int b = 0; b < length; b++) {
             for(int p = 0; p < scheme->data_wiping.length; p++) {
-                address[b] = scheme->data_wiping.pattern[p];
+                memcpy(&address[b], scheme->data_wiping.pattern[p], 1);
             }
         }
     }
