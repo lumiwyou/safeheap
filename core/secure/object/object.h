d5587d6b3721ab5a7d22fbc604887357ba5900e9
Lumi Hyväri <lumi.hyvari@gmail.com>
2026-05-02 01:09:18 +0000

    Added doxygen comments and `find_empty_context`

--- core/secure/object/object.h
+++ core/secure/object/object.h
@@ -15,6 +15,8 @@
 #include "../secure.h"
 #include "../../memory/memory.h"
 
+/// @brief The context of a memory object and the security operations surrounding it.
+/// Tracks the security schemes to be used, the memory units containing the memory object, and the SecureHandle.
 typedef struct {
     SecureHandle secure_handle;
     size_t data_total_size;
@@ -23,10 +25,33 @@ typedef struct {
     int memory_unit_count;
 } SecureObjectContext;
 
+/// @brief Central table for tracking all the SecureObjectContext entries
 extern SecureObjectContext ** secured_objects_table;
+/// @brief Tracks the count of SecureObjectContext entries
 extern int secured_objects_count;
 
+/// @brief Adds a new SecureObjectContext
+/// @param grade The sensitivity attribute to give
+/// @param size The size of memory object to give
+/// @return Secure handle to new context
 SecureHandle add_secure_object(Sensitivity grade, size_t size);
+
+/// @brief Removes SecureObjectContext by SecureHandle
+/// @param secure_handle The secure handle to match against
 void remove_secure_object(SecureHandle secure_handle);
+
+/// @brief Updates information in the SecureObjectContext
+/// Used in reallocation operations.
+/// @param secure_handle The secure handle used for matching
+/// @param grade The new sensitivity attribute
+/// @param size The new size attribute
 void update_secure_object(SecureHandle secure_handle, Sensitivity grade, size_t size);
+
+/// @brief Retrieves a SecureObjectContext by SecureHandle
+/// @param secure_handle The secure handle used for matching
+/// @return A pointer to the SecureObjectContext
 SecureObjectContext * get_context_by_handle(SecureHandle secure_handle);
+
+/// @brief Finds empty SecureObjectContext spots in the central table (secured_objects_table)
+/// @return A pointer to the context
+SecureObjectContext ** find_empty_context(void);
